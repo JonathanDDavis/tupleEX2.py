768a48eaea71624f3a0901c33ccb67a86a082b26
JonathanDDavis <125523206+JonathanDDavis@users.noreply.github.com>
2024-05-28 14:07:04 -0500

    Add files via upload

---
+++ tupleEX2.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      jd0919889
+#
+# Created:     31/10/2019
+# Copyright:   (c) jd0919889 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+name = "Bill"
+age = 45
+##print(name , "is" , age , "years old.")
+##print(name + " is " + str(age) + " years old.")
+##print("%s is %d years old." % (name, age))
+myArray = ["Knife", "Sword", "Potion", 50]
+print("You are carrying: %s and %d gold" % (myArray[0:3], myArray[3]))
